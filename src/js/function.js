71b102b1af952c45edc0923f5edc51ff070038a5
AlexCordero <alexanderj.cordero@gmail.com>
2016-11-25 22:26:00 -0400

    Prototipo inicial

---
+++ src/js/function.js
@@ -0,0 +1,8 @@
+    var drawer = document.querySelector('app-drawer');
+    document.querySelector('paper-icon-button').addEventListener('tap', function() {
+      drawer.toggle();
+    });
+      
+      document.querySelector('.cerrar').addEventListener('tap', function() {
+      drawer.toggle();
+    });
